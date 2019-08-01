ae57f3e46265a46de6cb9a5e17975479db5fbe34
aawoznia2644_do_github <ania.woznia@gmail.com_do_github>
2019-08-01 20:57:26 +0200

    First Commit

---
+++ src/main/java/com/kodilla/auxilium_front/AuxiliumFrontApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.auxilium_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuxiliumFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuxiliumFrontApplication.class, args);
+    }
+
+}
